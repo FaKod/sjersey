1f09427693e64727a8fed101141828372f918677
Christopher Schmidt <fakod666@googlemail.com>
2013-01-31 16:36:37 +0100

    added Factory and default settings

--- src/main/scala/eu/fakod/sjersey/util/JacksonDeAndSerializer.scala
+++ src/main/scala/eu/fakod/sjersey/util/JacksonDeAndSerializer.scala
@@ -1,10 +1,11 @@
 package eu.fakod.sjersey.util
 
-import com.fasterxml.jackson.databind.{ObjectMapper, Module}
+import com.fasterxml.jackson.databind.{MappingJsonFactory, ObjectMapper, Module}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import java.io.{OutputStream, InputStream}
 import java.lang.reflect.{ParameterizedType, Type}
 import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.core.{JsonParser, JsonGenerator}
 
 /**
  *
@@ -13,8 +14,15 @@ trait JacksonDeAndSerializer {
 
   val module: Module = DefaultScalaModule
 
-  def mapper = {
-    val result = new ObjectMapper
+  val mapper = {
+    val factory = new MappingJsonFactory
+    factory.enable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT)
+    factory.enable(JsonGenerator.Feature.AUTO_CLOSE_TARGET)
+    factory.enable(JsonGenerator.Feature.QUOTE_FIELD_NAMES)
+    factory.enable(JsonParser.Feature.ALLOW_COMMENTS)
+    factory.enable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
+
+    val result = new ObjectMapper(factory)
     result.registerModule(module)
     result
   }
